40b05b65b63e41f8124a53dc7496dd5d8d958c17
JenCastioni <jennifer.castioni@gmail.com>
2020-10-30 13:11:15 -0400

    Finished freecodecamp

---
+++ chunkymonkey.js
@@ -0,0 +1,42 @@
+// Basic Algorithm Scripting: Chunky Monkey:
+
+// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+
+// chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
+
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
+
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].
+
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
+
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].
+
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
+
+function chunkArrayInGroups(arr, size) {
+    let newArray = [];
+    while (arr.length > 0){
+      newArray.push(arr.splice(0, size))
+    }
+    return newArray;
+  }
+  
+  chunkArrayInGroups(["a", "b", "c", "d"], 2);
+  
+
+  // Solve with For Loop
+
+  function chunkArrayInGroups(arr, size) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i + size) {
+      newArray.push(arr.splice(i, size))
+    }
+    return newArray;
+  }
+  
+  chunkArrayInGroups(["a", "b", "c", "d"], 2);
+  
