e769a3896ad580459739d8ba5edaead6ba91d57c
JenCastioni <jennifer.castioni@gmail.com>
2020-11-09 14:00:26 -0500

    more practice with Fucntional Programming

---
+++ Functional-Programming/concat-method.js
@@ -0,0 +1,22 @@
+// Functional Programming: Combine Two Arrays Using the concat Method:
+
+// Concatenation means to join items end to end. JavaScript offers the concat method for both strings and arrays that work in the same way. For arrays, the method is called on one, then another array is provided as the argument to concat, which is added to the end of the first array. It returns a new array and does not mutate either of the original arrays.
+
+// Use the concat method in the nonMutatingConcat function to concatenate attach to the end of original. The function should return the concatenated array.
+
+// Your code should use the concat method.
+
+// The first array should not change.
+
+// The second array should not change.
+
+// nonMutatingConcat([1, 2, 3], [4, 5]) should return [1, 2, 3, 4, 5].
+
+function nonMutatingConcat(original, attach) {
+    let newArr = original.concat(attach)
+    return newArr
+  }
+  var first = [1, 2, 3];
+  var second = [4, 5];
+  nonMutatingConcat(first, second);
+  
